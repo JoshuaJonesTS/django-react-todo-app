4feb84b1f92fbd2f92b853ac299a90fd73111dc0
JoshuaJonesTS <joshua@tailoredsoftware.us>
2023-03-07 15:04:05 -0800

    Get token

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -19,7 +19,24 @@ class App extends React.Component {
     this.fetchTasks = this.fetchTasks.bind(this);
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.getCookie = this.getCookie.bind(this);
   }
+
+  getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
 
   componentWillMount() {
     this.fetchTasks()
@@ -54,11 +71,14 @@ class App extends React.Component {
     e.preventDefault();
     console.log("ITEM", this.state.activeItem);
 
+    var csrftoken = this.getCookie('csrftoken')
+
     var url = baseURL + '/api/task-create/';
     fetch(url, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
+        'X-CSRFToken': csrftoken
       },
       body: JSON.stringify(this.state.activeItem)
     })
