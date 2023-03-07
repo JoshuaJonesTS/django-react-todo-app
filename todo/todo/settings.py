02b60a5345e6d3861f09ebf9e290469c450103e5
JoshuaJonesTS <joshua@tailoredsoftware.us>
2023-03-06 21:15:56 -0800

    Make backend data accessible to frontend

--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -44,10 +44,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'api.apps.ApiConfig',
-    'rest_framework'
+    'rest_framework',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -136,4 +138,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'reactapp/build/static'),
+]
+
+CORS_ORIGIN_WHITELIST = [
+    "http://localhost:3000",
 ]
