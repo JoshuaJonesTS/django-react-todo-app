7aa44566bfe82e5ed146db1435974486ba908bb0
JoshuaJonesTS <joshua@tailoredsoftware.us>
2023-03-07 14:17:25 -0800

    Display each task
    Also, create an edit button and a delete button for each task

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -28,13 +28,14 @@ class App extends React.Component {
     fetch(baseAPI + '/api/task-list/')
     .then(response => response.json())
     .then(data => {
-      this.state({
+      this.setState({
         todoList:data
       })
     })
   }
 
   render() {
+    var tasks = this.state.todoList;
     return (
       <div className="container">
         <div id="task-container">
@@ -51,7 +52,21 @@ class App extends React.Component {
             </form>
           </div>
           <div  id="list-wrapper">         
-                
+            {tasks.map(function(task, index) {
+              return(
+                <div key={index} className="task-wrapper flex-wrapper">
+                  <div style={{flex:7}}>
+                    <span>{task.title}</span>
+                  </div>
+                  <div style={{flex:1}}>
+                    <button className='btn btn-sm btn-outline-info'>Edit</button>
+                  </div>
+                  <div style={{flex:1}}>
+                    <button className='btn btn-sm btn-outline-dark delete'>-</button>
+                  </div>
+                </div>
+              )
+            })}
           </div>
         </div>
       </div>
