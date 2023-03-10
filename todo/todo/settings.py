92d5f6ca457e2e65d9c2dd9c8fc4ea700ebaec40
JoshuaJonesTS <joshua@tailoredsoftware.us>
2023-03-09 18:51:59 -0800

    Switch databases
    Use PostgreSQL database instead of SQLite

--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -85,13 +85,23 @@ WSGI_APPLICATION = 'todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+DATABASES={
+   'default':{
+      'ENGINE':'django.db.backends.postgresql_psycopg2',
+      'NAME':'postgres',
+      'USER':'postgres',
+      'PASSWORD': env('PASSWORD'),
+      'HOST':'localhost',
+      'PORT':'5432',
+   }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
